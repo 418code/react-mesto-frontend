d9a985aca201d40c2186ebc948c7cc7397e14bd4
Maxim Tolstokory <maxim.code@outlook.com>
2021-09-21 22:02:09 -0800

    fix: turn on PopupWithForm native validation and button opacity on hover

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,10 +6,12 @@ export default function PopupWithForm(props) {
       <div className="page__container">
         <button className="popup__container-close-btn transparent transparent_amount_more" type="button" aria-label="Кнопка закрытия попапа"
          onClick={props.onClose}></button>
-        <form name={props.name} className="page__form" noValidate onSubmit={props.onSubmit}>
+        <form name={props.name} className="page__form" onSubmit={props.onSubmit}>
           <h2 className={`page__form-title ${props.onlyTitle ? 'page__form-title_onlytitle' : ''}`}>{props.formTitle}</h2>
           {props.children}
-          <button className="page__form-submit-btn" type="submit">{props.isSaving ? popupConfig.popupWithFormSavingText : props.submitButtonText}</button>
+          <button className="page__form-submit-btn transparent transparent_amount_much-more" type="submit">
+            {props.isSaving ? popupConfig.popupWithFormSavingText : props.submitButtonText}
+          </button>
         </form>
       </div>
     </div>
