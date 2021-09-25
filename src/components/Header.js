6a59f436ba4f6795fb5ede29524fee30ab7bd3c2
Maxim Tolstokory <maxim.code@outlook.com>
2021-09-25 14:38:58 -0800

    refactor: use Route in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,25 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 export default function Header(props) {
-  const location = useLocation();
-  const currentRouteElement = () => {
-    switch (location.pathname) {
-      case '/sign-up': return (<Link to="/sign-in"
-        className="page__link page__link_size_adaptive page__link_color_white transparent transparent_amount_more">Войти</Link>);
-      case '/sign-in': return (<Link to="/sign-up"
-        className="page__link page__link_size_adaptive page__link_color_white transparent transparent_amount_more">Регистрация</Link>);
-      case '/': return (<div className="header__container">
-                        <p className="header__text">{props.email && props.email}</p>
-                        <Link to="sign-in" onClick={props.onLogout}
-                          className="page__link page__link_size_adaptive page__link_color_gray transparent transparent_amount_more">Выйти</Link>
-                       </div>);
-      default:
-    }
-  };
-
   return (
     <header className="header body__element">
       <img src={logo} alt="логотип mesto Russia" className="header__logo" />
-      {currentRouteElement()}
+      <Switch>
+        <Route exact path="/">
+          <div className="header__container">
+            <p className="header__text">{props.email && props.email}</p>
+            <Link to="sign-in" onClick={props.onLogout}
+              className="page__link page__link_size_adaptive page__link_color_gray transparent transparent_amount_more">Выйти</Link>
+          </div>
+        </Route>
+        <Route path="/sign-up">
+          <Link to="/sign-in" className="page__link page__link_size_adaptive page__link_color_white transparent transparent_amount_more">Войти</Link>
+        </Route>
+        <Route path="/sign-in">
+          <Link to="/sign-up" className="page__link page__link_size_adaptive page__link_color_white transparent transparent_amount_more">Регистрация</Link>
+        </Route>
+      </Switch>
     </header>
   );
 }
